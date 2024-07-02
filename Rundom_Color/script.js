5ac109a6d70b286cf3152dc9f6c1d974c9492241
Polina-2112 <polinapolina21@inbox.ru>
2024-07-02 17:45:19 +0300

    2-07

---
+++ Rundom_Color/script.js
@@ -0,0 +1,11 @@
+var W = document.getElementById("W");
+var H = document.getElementById("H");
+var B = document.getElementById("B");
+
+window.addEventListener('load', () => 
+{
+    W.placeholder = "Ширина";
+    H.placeholder = "Высота";
+    B.value = "Случайный цвет";
+ });
+
