4828c21d5da572b7ecd7dd87473459e75b88d074
Polina P <polinapolina21@inbox.ru>
2024-07-03 21:20:37 +0300

    3-21-20

--- Order/script.js
+++ Order/script.js
@@ -1,11 +1,40 @@
 
 var long, lat, myMap;
-function init()
+
+function init() 
 {
-    myMap = new ymaps.Map("map", {
-    center: [54.167324, 37.584789],
-            zoom: 15
-        });
+        myMap = new ymaps.Map("map", {
+        center: [54.167324, 37.584789],
+        zoom: 17
+    });
+
+    myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) 
+        {
+            var coords = e.get('coords');
+            ymaps.geocode(coords).then(function (res) 
+            {
+                var firstGeoObject = res.geoObjects.get(0);
+                var address = firstGeoObject.getAddressLine();
+                myMap.balloon.open(coords, {
+                    contentHeader: address,
+                    contentBody: '<p>Мы привезем заказ сюда: ' + [
+                        coords[0].toPrecision(6),
+                        coords[1].toPrecision(6) ].join(', ') + '</p>',
+                    contentFooter: '<sup>Щелкните еще раз, чтобы закрыть</sup>'
+                });
+
+                myPlacemark = new ymaps.Placemark([coords[0].toPrecision(6), coords[1].toPrecision(6)], 
+                { hintContent: address });
+
+                myMap.geoObjects.add(myPlacemark);
+            });
+        } 
+        else {
+            myMap.balloon.close();
+            myMap.geoObjects.remove(myPlacemark);
+        }
+    });
 }
 navigator.geolocation.getCurrentPosition(show, error, { maximumAge: 0, timeout: 5000, enableHighAccuracy: true });
 function show(position)
@@ -13,15 +42,12 @@ function show(position)
     long = position.coords.longitude;
     lat = position.coords.latitude;
     myMap.setCenter([lat, long]);
-    console.log("show");
-    console.log(lat); console.log(long); 
 }
 function error(positionError)
 {
     long = 54.167324;
     lat = 37.584789;
     myMap.setCenter([lat, long]);
-    console.log("error")
 }
 ymaps.ready(init);
 
@@ -40,8 +66,8 @@ function Button_Clicked()
 
         if (email.value.includes("@") == false && email.value != "") { temp = "Email должен содержать символ '@'!"; f = false; }
 
-        if (phone.value.trim() == "") { temp = "Впишите ваш номер телефона!"; f = false; }
-        for (let i = 0; i < phone.value.length; i++) { if (!(/\d/.test(phone.value[i]))) { temp = "Номер телефона не может содержать цифры!"; break; f = false; } }
+        if (phone.value.trim() == "") { temp = "Введите ваш номер телефона!"; f = false; }
+        for (let i = 0; i < phone.value.length; i++) { if (!(/\d/.test(phone.value[i]))) { temp = "Номер телефона не может содержать цифры!"; f = false; break;  } }
 
         if (fio.value.trim() == "") { temp = "Заполните ФИО!"; f = false; }
         for (let i = 0; i < fio.value.length; i++) { if (/\d/.test(fio.value[i])) { temp = "ФИО не должно содержать цифр!"; break; f = false; } }   
@@ -49,6 +75,5 @@ function Button_Clicked()
         if (f) 
             { temp = "Ваш заказ успешно оформлен!"; mes.style.color = "#04DB00"; }
 
-        console.log(temp);
         mes.textContent = temp;
     };
