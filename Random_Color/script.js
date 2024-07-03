2c20399577a12e1da8bd4fa815c5896c121cd788
Polina P <polinapolina21@inbox.ru>
2024-07-03 11:06:33 +0300

    03-11-06

--- Random_Color/script.js
+++ Random_Color/script.js
@@ -1,11 +1,20 @@
 var W = document.getElementById("W");
 var H = document.getElementById("H");
 var B = document.getElementById("B");
+var R = document.getElementById("rect");
 
 window.addEventListener('load', () => 
 {
     W.placeholder = "Ширина";
     H.placeholder = "Высота";
     B.value = "Случайный цвет";
- });
+});
 
+W.addEventListener("input", () => 
+{ R.style.width = W.value + "px"; });
+
+H.addEventListener("input", () => 
+{ R.style.height = H.value  + "px"; });
+
+B.addEventListener("click", () =>
+{ R.style.background = "#" + Math.trunc(Math.random() * 0xFFFFFF); });
